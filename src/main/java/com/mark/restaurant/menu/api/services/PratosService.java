410cb14ce7f169db49e450694d8382f0c7e6fa6f
immark007 <immark5507@gmail.com>
2025-02-04 19:58:27 -0300

    feat: doing simple checks on my PratosService

--- src/main/java/com/mark/restaurant/menu/api/services/PratosService.java
+++ src/main/java/com/mark/restaurant/menu/api/services/PratosService.java
@@ -3,12 +3,16 @@ package com.mark.restaurant.menu.api.services;
 import com.mark.restaurant.menu.api.domain.Cardapio;
 import com.mark.restaurant.menu.api.domain.Pratos;
 import com.mark.restaurant.menu.api.dto.CreatePratoDto;
+import com.mark.restaurant.menu.api.exceptions.BadRequestExecption;
+import com.mark.restaurant.menu.api.exceptions.NotFoundException;
 import com.mark.restaurant.menu.api.repositories.CardapioRepository;
 import com.mark.restaurant.menu.api.repositories.PratosRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 
 @Service
 public class PratosService {
@@ -18,16 +22,24 @@ public class PratosService {
     @Autowired
     private CardapioRepository cardapioRepository;
 
+    @Transactional
     public Pratos savePratos(CreatePratoDto createPratoDto) {
         Cardapio cardapio = cardapioRepository.findById(createPratoDto.cardapioId())
-                .orElseThrow(() -> new RuntimeException("Cardápio não encontrado"));
+                .orElseThrow(() -> new BadRequestExecption("Cardápio não encontrado"));
 
         Pratos prato = new Pratos(createPratoDto.nome(), createPratoDto.imagemUrl(), createPratoDto.price(), createPratoDto.tipo(), cardapio);
 
         return pratosRepository.save(prato);
     }
 
-    public List<Pratos> findAllPratos() {
-        return pratosRepository.findAll();
+    public Page<Pratos> findAllPratos(Pageable pageable) {
+        return pratosRepository.findAll(pageable);
+    }
+
+    public void deletePrato(Long id) {
+        if(!pratosRepository.existsById(id)) {
+            throw new NotFoundException("Prato não encontrado");
+        }
+        pratosRepository.deleteById(id);
     }
 }
