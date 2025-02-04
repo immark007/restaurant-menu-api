7bec946e840f9dfb1395fa6a62b5e4389236201e
immark007 <immark5507@gmail.com>
2025-02-04 14:29:37 -0300

    feat: initial project

---
+++ src/main/java/com/mark/restaurant/menu/api/RestaurantMenuApiApplication.java
@@ -0,0 +1,13 @@
+package com.mark.restaurant.menu.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantMenuApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantMenuApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mark/restaurant/menu/api/RestaurantMenuApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mark.restaurant.menu.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantMenuApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
